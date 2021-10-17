e1041828436bcdc7e06c3a0de78f96f0bc1c5b05
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-10-17 19:47:58 +0600

    added DB connector

---
+++ Model/DbConnector.php
@@ -0,0 +1,20 @@
+<?php
+
+//Function to connect to database
+function db_connect(){
+    $servername = "144.208.125.220";
+    $username = "alvihasa_CarRental";
+    $password = "CarRental1";
+    $dbname = "alvihasa_CarRentalDB";
+
+    $connection = new mysqli($servername, $username, $password, $dbname) or die("Could not connect to DataBase ". $connection -> connect_error);
+
+    return $connection;
+}
+
+//Function to close connection to database
+function Dispose($connection) {
+    $connection -> close();
+}
+
+?>
