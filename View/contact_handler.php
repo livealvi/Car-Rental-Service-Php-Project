fdee848cde78ab74a5297d3ec2de405854d183c1
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-10-21 17:21:26 +0600

    added contact section

---
+++ View/contact_handler.php
@@ -0,0 +1,3 @@
+<?php
+if(isset($_POST['submit'])) echo 'page reached';
+else echo 'submit empty';
--- View/welcome.php
+++ View/welcome.php
@@ -116,11 +116,106 @@
                     </td>
                 </tr>
                 <!--section padding bottom End-->
+
+
             </table>
         </td>
     </tr>
 </table>
 <!--About section end-->
+
+<!--Contact section start-->
+<form method="post" action="contact_handler.php">
+<table border="0" id="contact" width="100%" cellpadding="0" cellspacing="0" bgcolor="#b8e3ff">
+    <tr>
+        <td>
+            <table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+                <!-- Heading Start-->
+                <tr>
+                    <td height="160" align="center" valign="middle">
+                        <font face="arial" size="6" color=" #000000">
+                            Contact
+                        </font>
+                        <hr width="70" color="#000000">
+                    </td>
+                </tr>
+                <!-- Heading  End-->
+                <tr>
+                    <td align="center" valign="top">
+                        <table border="0" width="55%" cellpadding="20" cellspacing="0" align="center" bgcolor="#0077b6">
+                            <tr>
+                                <td>
+                                    <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center" bgcolor="#0077b6">
+                                        <tr>
+                                            <td width="30%">
+                                                <font face="arial" size="4" color="#ffffff">
+                                                    Name
+                                                </font>
+                                            </td>
+                                            <td width="70%">
+                                                <input type="text" size="50">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%">
+                                                <font face="arial" size="4" color="#ffffff">
+                                                    Email
+                                                </font>
+
+                                            </td>
+                                            <td width="70%">
+                                                <input type="text" size="50">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%">
+                                                <font face="arial" size="4" color="#ffffff">
+                                                    Subject
+                                                </font>
+                                            </td>
+                                            <td width="70%">
+                                                <input type="text" size="50">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%">
+                                                <font face="arial" size="4" color="#ffffff">
+                                                    Message
+                                                </font>
+                                            </td>
+                                            <td width="70%">
+                                                <textarea cols="51" rows="4"></textarea>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%">
+                                                &nbsp;
+                                            </td>
+                                            <td width="70%">
+                                                <button type="submit" name="submit" value="contact">
+                                                    <font size="5" face="arial">Send Message</font>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <!-- section padding bottom -->
+                <tr>
+                    <td height="60">
+
+                    </td>
+                </tr>
+                <!-- section padding bottom End-->
+            </table>
+        </td>
+    </tr>
+</table>
+</form>
+<!--Contact section end-->
 
 <!--Footer inclusion-->
 <?php include 'footer.php' ?>
