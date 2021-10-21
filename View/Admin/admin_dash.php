e45a5c2389a5c81948da2ee6c67fc1dc966efa56
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-10-22 02:16:09 +0600

    added admin files

---
+++ View/Admin/admin_dash.php
@@ -0,0 +1,56 @@
+<!--Landing page-->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin dashboard</title>
+</head>
+<body>
+<!--Header inclusion-->
+<?php include '../header.php' ?>
+
+<!--Main table structure-->
+<table border="0" id="home" width="100%" cellpadding="0" cellspacing="0" bgcolor="#b8e3ff">
+    <tr>
+        <td >
+            <?php include 'admin_side_panel.php' ?>
+        </td>
+        <td>
+            <table border="0" width="100%" cellpadding="15" cellspacing="0" align="center">
+                <tr>
+                    <td align="center" valign="middle" height="300">
+                        <h3>
+                            <font face="arial" color="#ffffff" size="6">
+                                Welcome to
+                            </font>
+                        </h3>
+                        <h1>
+                            <font face="arial" color="#000000" size="7">
+                                Elite Rentals
+                            </font>
+                        </h1>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+<!--Main table structure end-->
+
+<!-- section padding bottom -->
+<tr>
+    <td height="60">
+
+    </td>
+</tr>
+<!-- section padding bottom End-->
+
+
+<!--Footer inclusion-->
+<?php include '../footer.php' ?>
+</body>
+</html>
---
+++ View/Admin/admin_side_panel.php
@@ -0,0 +1,79 @@
+<table border="0" width="60%" cellpadding="15" cellspacing="0" align="left" height="600" bgcolor="#0077b6">
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button >
+                    <font face="arial" color="#000000" size="4">
+                        Renter list
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        Car Owner list
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        Employee information
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        Car information
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        Rented cars' information
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        Car documents
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" valign="middle">
+            <a href="">
+                <button>
+                    <font face="arial" color="#000000" size="4">
+                        User documents
+                    </font>
+                </button>
+            </a>
+        </td>
+    </tr>
+</table>
