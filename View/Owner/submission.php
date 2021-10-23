7ae1ac725c75f2ef04ea22c2432592f7746d593a
shihab2213 <58392572+shihab2213@users.noreply.github.com>
2021-10-23 22:44:51 +0600

    Add files via upload

---
+++ View/Owner/submission.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<body>
+        <h1>Sussessfully Submitted!</h1>
+        <br>
+
+        <form action="login.php" method="POST">
+        <input type="submit" name="submit" value="Home">
+            
+             </form>
+
+        </body>
+</html>      
