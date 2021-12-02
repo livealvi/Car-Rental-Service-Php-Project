999c3f2772ab08fd62f1d057c568db64120f18f0
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-12-02 15:27:17 +0600

    added new header style

--- View/header.php
+++ View/header.php
@@ -5,103 +5,35 @@ if(isset($_SESSION['user_type'])){
     if($_SESSION['user_type'] == 'admin') $account_forward_url = 'Admin/admin_dash.php';
     else if($_SESSION['user_type'] == 'renter') $account_forward_url = '';
     else if($_SESSION['user_type'] == 'owner') $account_forward_url = '';
-    else if($_SESSION['user_type'] == 'employee') $account_forward_url = 'Employee/emp_dashboard.php';
+    else if($_SESSION['user_type'] == 'employee') $account_forward_url = 'Employee/dashboard/emp_dashboard.php';
 }
 if (isset($_SESSION['user_name'])) {
 ?>
-    <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#3d5a80">
-        <tr>
-            <td>
-                <table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
-                    <tr>
-                        <td>
-                            <font face="arial" color="black" size="5">
-                                <strong>Elite Rentals</strong>
-                            </font>
-                        </td>
-                        <td width="30%">&nbsp;</td>
-                        <td><a href="../index.php">
-                                <font face="arial" color="#ffffff" size="3">
-                                    Home
-                                </font></a>
-                        </td>
-<!--                        <td><a href="#about">-->
-<!--                                <font face="arial" color="#ffffff" size="3">-->
-<!--                                    About-->
-<!--                                </font></a>-->
-<!--                        </td>-->
-<!--                        <td><a href="#contact">-->
-<!--                                <font face="arial" color="#ffffff" size="3">-->
-<!--                                    Contact-->
-<!--                                </font></a>-->
-<!--                        </td>-->
-                        <td>
-                                <font face="arial" color="#ffffff" size="3">
-                                    Signed in as: <a href="<?php echo $account_forward_url ?>">
-                                        <font face="arial" color="#ffffff" size="3"> <?php echo $_SESSION['user_name']?></font>
-                                    </a>
-                                </font>
-                        </td>
-                        <td><a href="../Controller/logout_handler.php">
-                                <font face="arial" color="#ffffff" size="3">
-                                    Logout
-                                </font></a>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    <ul class="navbar">
+        <li id="logo"><h2>Elite Rentals</h2></li>
+        <ul class="dropdown">
+            <button class="dropbtn">Signed in as: <span> - </span> <?php echo $_SESSION['user_name'] ?></button>
+            <div class="dropdown-content">
+                <a href=../Controller/logout_handler.php>Logout</a>
+            </div>
+        </ul>
+    </ul>
 
 <?php } else {
 ?>
-    <table border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#3d5a80">
-        <tr>
-            <td>
-                <table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
-                    <tr>
-                        <td>
-                            <font face="arial" color="black" size="5">
-                                <strong>Elite Rentals</strong>
-                            </font>
-                        </td>
-                        <td width="30%">&nbsp;</td>
-                        <td><a href="welcome.php">
-                                <font face="arial" color="#ffffff" size="3">
-                                    Home
-                                </font></a>
-                        </td>
-                        <td><a href="#about">
-                                <font face="arial" color="#ffffff" size="3">
-                                    About
-                                </font></a>
-                        </td>
-                        <td><a href="#contact">
-                                <font face="arial" color="#ffffff" size="3">
-                                    Contact
-                                </font></a>
-                        </td>
-                        <td><a href="login.php">
-                                <button>
-                                    <font face="arial" color="#000" size="3">
-                                        Sign in
-                                    </font>
-                                </button>
-                            </a>
-                        </td>
-                        <td><a href="register.php">
-                                <button>
-                                    <font face="arial" color="#000" size="3">
-                                        Register
-                                    </font>
-                                </button>
-                            </a>
-                        </td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+    <ul class="navbar">
+        <li id="logo"><h2>Elite Rentals</h2></li>
+        <div id="nav-right">
+            <ul class="navbar">
+                <li><a href="#">Home</a></h6></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="login.php"><button>Sign in</button></a></li>
+                <li><a href="register.php"><button>Register</button></a></li>
+            </ul>
+        </div>
+    </ul>
+
 <?php
 }
 ?>
--- View/welcome.php
+++ View/welcome.php
@@ -6,11 +6,8 @@ session_start();
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to Elite Rentals</title>
+    <link rel="stylesheet" href="css/header.css">
 </head>
 <body>
 <!--Header inclusion-->
