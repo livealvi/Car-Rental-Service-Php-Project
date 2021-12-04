a8bbb280e62c94943ca5b291a10c088d14cb811c
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-12-04 21:40:57 +0600

    changed header styling

--- View/header.php
+++ View/header.php
@@ -9,31 +9,34 @@ if(isset($_SESSION['user_type'])){
 }
 if (isset($_SESSION['user_name'])) {
 ?>
-    <ul class="navbar">
-        <li id="logo"><h2>Elite Rentals</h2></li>
-        <ul class="dropdown">
-            <button class="dropbtn">Signed in as: <span> - </span> <?php echo $_SESSION['user_name'] ?></button>
-            <div class="dropdown-content">
-                <a href=../Controller/logout_handler.php>Logout</a>
-            </div>
+    <div class="nav-wrap">
+        <ul class="navbar">
+            <li id="logo"><h2>Elite Rentals</h2></li>
+            <ul class="dropdown">
+                <button class="dropbtn">Signed in as: <span> - </span> <?php echo $_SESSION['user_name'] ?></button>
+                <div class="dropdown-content">
+                    <a href=../Controller/logout_handler.php>Logout</a>
+                </div>
+            </ul>
         </ul>
-    </ul>
+    </div>
 
 <?php } else {
 ?>
-    <ul class="navbar">
-        <li id="logo"><h2>Elite Rentals</h2></li>
-        <div id="nav-right">
-            <ul class="navbar">
-                <li><a href="#home">Home</a></h6></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="login.php"><button class="header-btn">Sign in</button></a></li>
-                <li><a href="register.php"><button class="header-btn">Register</button></a></li>
-            </ul>
-        </div>
-    </ul>
-
+    <div class="nav-wrap">
+        <ul class="navbar">
+            <li id="logo"><h2>Elite Rentals</h2></li>
+            <div id="nav-right">
+                <ul class="navbar">
+                    <li><a href="welcome.php">Home</a></h6></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="login.php"><button class="header-btn">Sign in</button></a></li>
+                    <li><a href="register.php"><button class="header-btn">Register</button></a></li>
+                </ul>
+            </div>
+        </ul>
+    </div>
 <?php
 }
 ?>
