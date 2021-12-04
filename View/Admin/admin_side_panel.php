9b6113e0219a70986e41f93f5148420ff6f33515
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-12-05 02:09:06 +0600

    changed admin dash

--- View/Admin/admin_side_panel.php
+++ View/Admin/admin_side_panel.php
@@ -1,112 +1,20 @@
-<table border="0" width="400px" cellpadding="15" cellspacing="0" align="left" height="600" bgcolor="#0077b6">
-    <tr>
-        <td align="center" valign="middle">
-            <a href="admin_dash.php">
-                <button >
-                    <font face="arial" color="#000000" size="4">
-                        Dash Board
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td align="center" valign="middle">
-            <a href="renter_list_page.php">
-                <button >
-                    <font face="arial" color="#000000" size="4">
-                        Renter list
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td align="center" valign="middle">
-            <a href="owner_list_page.php">
-                <button>
-                    <font face="arial" color="#000000" size="4">
-                        Car Owner list
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td align="center" valign="middle">
-            <a href="employee_list_page.php">
-                <button>
-                    <font face="arial" color="#000000" size="4">
-                        Employee List
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td align="center" valign="middle">
-            <a href="car_list_page.php">
-                <button>
-                    <font face="arial" color="#000000" size="4">
-                        Car List
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <td align="center" valign="middle">
-            <a href="booking_list_page.php">
-                <button>
-                    <font face="arial" color="#000000" size="4">
-                        Bookings list
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-<!--    <tr>-->
-<!--        <td align="center" valign="middle">-->
-<!--            <a href="">-->
-<!--                <button>-->
-<!--                    <font face="arial" color="#000000" size="4">-->
-<!--                        Car documents-->
-<!--                    </font>-->
-<!--                </button>-->
-<!--            </a>-->
-<!--        </td>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td align="center" valign="middle">-->
-<!--            <a href="">-->
-<!--                <button>-->
-<!--                    <font face="arial" color="#000000" size="4">-->
-<!--                        User documents-->
-<!--                    </font>-->
-<!--                </button>-->
-<!--            </a>-->
-<!--        </td>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td align="center" valign="middle">-->
-<!--            <a href="">-->
-<!--                <button>-->
-<!--                    <font face="arial" color="#000000" size="4">-->
-<!--                        Employee documents-->
-<!--                    </font>-->
-<!--                </button>-->
-<!--            </a>-->
-<!--        </td>-->
-<!--    </tr>-->
-    <tr>
-        <td align="center" valign="middle">
-            <a href="feedback_list_page.php">
-                <button>
-                    <font face="arial" color="#000000" size="4">
-                        User feedbacks
-                    </font>
-                </button>
-            </a>
-        </td>
-    </tr>
-</table>
+<div class="sidenav">
+    <div class="nav-element">
+        <a href="admin_dash.php">Dashboard</a>
+    </div>
+    <div class="nav-element">
+        <a href="car_list_page.php">Car List</a>
+    </div>
+    <div class="nav-element">
+        <a href="renter_list_page.php">Renter List</a>
+    </div>
+    <div class="nav-element">
+        <a href="owner_list_page.php">Car Owners List</a>
+    </div>
+    <div class="nav-element">
+        <a href="booking_list_page.php">Bookings List</a>
+    </div>
+    <div class="nav-element">
+        <a href="feedback_list_page.php">User Feedbacks</a>
+    </div>
+</div>
