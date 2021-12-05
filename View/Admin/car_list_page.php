527d358169b8031a3d680d2e07385640097bfeee
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-12-05 15:17:21 +0600

    added new styling

--- View/Admin/car_list_page.php
+++ View/Admin/car_list_page.php
@@ -12,11 +12,10 @@ include '../../Controller/AdminController/car_list_handler.php';
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Car List</title>
+    <link rel="stylesheet" href="css/admin_header.css">
+    <link rel="stylesheet" href="css/admin_footer.css">
+    <link rel="stylesheet" href="css/admin_side_panel.css">
 </head>
 <body>
 <!--Header inclusion-->
