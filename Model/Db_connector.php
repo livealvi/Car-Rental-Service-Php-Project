324a25820730cbe5a29fe9d28db96ed075f904f2
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-12-11 01:20:19 +0600

    Add files via upload

---
+++ Model/Db_connector.php
@@ -0,0 +1,27 @@
+<?php
+//database accessor class
+class Db
+{
+
+    //Function to connect to database
+    function open_con()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "276914049";
+        $dbname = "alvihasa_CarRentalServiceDB";
+        $connection = new mysqli($servername, $username, $password, $dbname);
+
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        } else {
+            return $connection;
+        }
+    }
+
+    //Function to close connection to database
+    function close_con($connection)
+    {
+        $connection->close();
+    }
+}
