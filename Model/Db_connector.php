ce5f7050eaa6c74ffde624badbfab5c7caa251bd
Alvi Hasan <livealvi@gmail.com>
2021-12-11 00:39:42 +0600

    update DB

--- Model/Db_connector.php
+++ Model/Db_connector.php
@@ -9,7 +9,8 @@ class Db
         $servername = "localhost";
         $username = "root";
         $password = "276914049";
-        $connection = new mysqli($servername, $username, $password);
+        $dbname = "alvihasa_CarRentalServiceDB";
+        $connection = new mysqli($servername, $username, $password, $dbname);
 
         if ($connection->connect_error) {
             die("Connection failed: " . $connection->connect_error);
